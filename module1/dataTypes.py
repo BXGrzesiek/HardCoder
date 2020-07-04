54b5b1d470c45bc8233812293c223fa2e4fc364f
BXGrzesiek <grzegorz.biskup2015@gmail.com>
2020-07-04 11:04:14 +0200

    dataTypes
    data types

---
+++ module1/dataTypes.py
@@ -0,0 +1,18 @@
+#dataTypes
+#print(1+"'st module")
+#error unsupported operand type(s) for +: 'int' and 'str'ing
+#rzutowanie int -> string
+print(str(1) + "'st module")
+#rzutowanie string -> int
+
+#dzwonek
+print("\a Czesc BXGrzesiek")
+
+#druk backslash'a
+print("\t\\BXGrzesiek\\")
+#combo
+print("My favourite cars are \\\"Subaru Impreza\" & \"Nissan Skyline R34 GT-R\\\"")
+
+#dwa printy bez przejścia do nowej linii
+print("NISSAN", end="")
+print(" SKYLINE")
