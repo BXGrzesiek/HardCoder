48efaf2b7f8015c62bb1520a7e10cd2353963bc2
BXGrzesiek <grzegorz.biskup2015@gmail.com>
2020-07-04 10:36:05 +0200

    string_number
    string_number

---
+++ module1/helloWorld.py
@@ -0,0 +1,12 @@
+# Tutaj pisz swój kod, młody padawanie ;-)
+print(2+2.0)
+#obliczenia trudniejsze
+print(3*15*99/121+3)
+#łączenie stringów
+print("Grzegorz"+"Biskup")
+#ze spacją
+print("Mam " + "Psa")
+#mnożenie napisów
+print("HA"*3)
+#tylko liczby całkowite
+print("HA"*2.5) #error
