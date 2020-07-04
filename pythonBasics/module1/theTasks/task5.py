03b5adf2a407f2c8c93296699ef059eb5c869289
BXGrzesiek <grzegorz.biskup2015@gmail.com>
2020-07-04 14:03:09 +0200

    create task5
    create task5.py

---
+++ pythonBasics/module1/theTasks/task5.py
@@ -0,0 +1,4 @@
+#Write in one line the instruction that will calculate the result of operation 55*66*77-3,
+#and then displays the text "The result of the action is: <numeric result should appear here>"
+
+print("The result of the action is: " + str(float(((55*66)*77)-3)))
