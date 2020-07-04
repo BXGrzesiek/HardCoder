48d4ca516a32a0a0481080c9b22369fc5a8f0b62
BXGrzesiek <grzegorz.biskup2015@gmail.com>
2020-07-04 11:36:37 +0200

    updateVariables.py
    updateVariables.py

--- module1/variables.py
+++ module1/variables.py
@@ -1,6 +1,20 @@
 #variables - boxes for datas
-print("Put your name here: ")
-user_name=input()
-print("How old are you? ")
-user_age=input()
-print("Hi, " + user_name + "(" + user_age + ")")
+#    print("Put your name here: ")
+#    user_name=input()
+#    print("How old are you? ")
+#    user_age=input()
+#    print("Hi, " + user_name + "(" + user_age + ")")
+
+#keyword list
+#    import keyword
+#    print(keyword.kwlist)
+
+#ex1 0.5*((a+b)*h)
+print("Program liczący pole trapezu.\nPodaj długość A: ")
+a=input()
+print("Podaj długość B: ")
+b=input()
+print("Podaj długość H: ")
+h=input()
+print("Pole trapezu to: ")
+print(((int(a)+int(b))*int(h))/2)
