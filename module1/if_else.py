543d0c9aee054d1ed3a6d39442dcc0e8c2bbd45e
BXGrzesiek <grzegorz.biskup2015@gmail.com>
2020-07-04 12:04:13 +0200

    if-else
    if-else conditional statements

---
+++ module1/if_else.py
@@ -0,0 +1,26 @@
+#print("Porównywarka zmiennych!\nPodaj A: "+ end="")
+#a=input()
+#print("Podaj B: ", end="")
+#b=input()
+
+#if a==b:
+#    print("A=B")
+#else:
+#    print("A jest ! od B")
+
+#ex1 - check user_age - can user play in Wiedzmin game
+#print("Czy mogę grać w wiedźmina?\nPodaj wiek: ", end="")
+#age=input()
+#if int(age)>18:
+#    print("Możesz grać w Wiedźmina")
+#elif int(age)==18:
+#    print("Gratuluję w tym roku uzyskałeś uprawnienia")
+#else:
+#    print("Nie możesz grać w wiedźmina jeszcze")
+
+#ex2 - check result for y>10 and x<5 or z=100
+x=4
+y=5
+z=50
+
+print(y>10 and x<5 or z==100)
---
+++ module1/logicalComparison.py
@@ -0,0 +1,6 @@
+#porownanie x==y
+#    105==100 #true / false
+#    print("napis" == "Napis") #true / false
+
+#    print(500 <= "500") #fatal error
+print(500 == "500") #no error just FALSE
