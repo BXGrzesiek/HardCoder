3a3d0749f8d8d8c288f6d9b5c3d99f23df1207a9
BXGrzesiek <grzegorz.biskup2015@gmail.com>
2020-07-04 13:13:10 +0200

    create task2
    done task2.py

---
+++ pythonBasics/module1/theTasks/task2.py
@@ -0,0 +1,6 @@
+#Write a program in which you enter your annual age, but the year of birth is displayed.
+print("How old are you? : ", end="")
+age=input()
+year=2020-int(age)
+print("Your birth year is: ", end="")
+print(int(year))
