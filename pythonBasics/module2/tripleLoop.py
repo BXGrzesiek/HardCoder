9f99be022ee13f7e68a11a1862892a9a3c9855d6
BXGrzesiek <grzegorz.biskup2015@gmail.com>
2020-07-15 00:39:15 +0200

    Create tripleLoop.py
    create tripleLoop.py

---
+++ pythonBasics/module2/tripleLoop.py
@@ -0,0 +1,13 @@
+for x in range(10):
+    for y in range(10):
+        for z in range(5):
+            print("Piksel: "
+            + str(x)
+            + ","
+            + str(y)
+            + ","
+            + str(z))
+        if (x==4 and y==3 and z==3)
+            print("Przeciwnik!")
+
+            zmienić nazwę tripleLoop.py
