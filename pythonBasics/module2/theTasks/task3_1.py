f6fb9c387e49988d6ca9d60a5b7ca0eede25de16
BXGrzesiek <grzegorz.biskup2015@gmail.com>
2020-07-14 19:52:15 +0200

    create task3_1
    create task3_1.py

---
+++ pythonBasics/module2/theTasks/task3_1.py
@@ -0,0 +1,11 @@
+tab = [5, 15, 1, 25, 2]
+# tab[0] => 5
+# tab[2] => 1
+# tab[-1] => 2
+# tab[-3] => 1
+# tab[10] => ERROR: out of range
+# len(tab) => 5
+# max(tab) => 25
+# sum(tab) => 48
+# sorted(tab) => [1, 2, 5, 15, 25]
+# sorted(tab, reverse = True) => [25, 15, 5, 2, 1]
