42a0ae3ce753a0be00e67d3887a098f9033ce8bf
BXGrzesiek <grzegorz.biskup2015@gmail.com>
2020-07-15 01:30:59 +0200

    Update tripleLoop.py
    update tripleLoop.py

--- pythonBasics/module2/tripleLoop.py
+++ pythonBasics/module2/tripleLoop.py
@@ -1,13 +1,22 @@
+# How loop nesting works
+
+print("List of coordinates.")
 for x in range(10):
     for y in range(10):
         for z in range(5):
-            print("Piksel: "
+            print("\nPiksel: "
             + str(x)
             + ","
             + str(y)
             + ","
-            + str(z))
-        if (x==4 and y==3 and z==3)
-            print("Przeciwnik!")
-
-            zmienić nazwę tripleLoop.py
+            + str(z), end="")
+            if (int(x) == 1 and int(y) == 2 and int(z) == 3):
+                print(" ==> Enemy!")
+            elif (x == 6 and y == 5 and z == 4):
+                print(" ==> Enemy!")
+            elif (x == 7 and y == 8 and z == 0):
+                print(" ==> Enemy!")
+            elif (x == 9 and y == 7 and z == 1):
+                print(" ==> Friend!")
+            else:
+                continue
