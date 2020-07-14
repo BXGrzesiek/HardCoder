64e4c1f592622919f80f2079f17a2e75c8bf1e40
BXGrzesiek <grzegorz.biskup2015@gmail.com>
2020-07-14 23:18:32 +0200

    Update logicalComparison.py
    notes:
    - - > update logical comparison of 2 different types of data < - -

--- pythonBasics/module1/logicalComparison.py
+++ pythonBasics/module1/logicalComparison.py
@@ -1,6 +1,5 @@
-#porownanie x==y
+#comparison x==y
 #    105==100 #true / false
-#    print("napis" == "Napis") #true / false
-
+#    print("strings" == "Strings") #true / false
 #    print(500 <= "500") #fatal error
 print(500 == "500") #no error just FALSE
