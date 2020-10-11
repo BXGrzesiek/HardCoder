b9e326b60d2ca4d1a8995ef6a2a2395091d7d3b8
BXGrzesiek <grzegorz.biskup2015@gmail.com>
2020-10-12 01:57:01 +0200

    update
    update

--- pythonAdvanced/module1/tests/comprehensionList.py
+++ pythonAdvanced/module1/tests/comprehensionList.py
@@ -4,5 +4,5 @@
 #   - and the string "haha" for
 #   - even numbers in this range.
 
-my_list = [-x**2 if x % 2 != 0 else "HAHA" for x in range(5, 20)]
+my_list = [-x**2 if x % 2 != 0 else "HAHA" for x in range(5, 21)]
 print(my_list)
